cd39f491b0d949618e2a6ff2626eaa304cc8bf15
Нурсултан Батырканов <artez994@gmail.com>
2018-06-21 17:10:26 +0600

    Design and small fixes

--- RedPetroleum/Models/Repositories/EmployeeRepository.cs
+++ RedPetroleum/Models/Repositories/EmployeeRepository.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Threading.Tasks;
 using System.Linq;
-using PagedList;
+using X.PagedList;
 
 namespace RedPetroleum.Models.Repositories
 {
