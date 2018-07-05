0817c94782e5dac0f0a9cd5168312814e5df5bf3
Нурсултан Батырканов <artez994@gmail.com>
2018-07-05 09:08:06 +0600

    editedModels

--- RedPetroleum/Models/Entities/Position.cs
+++ RedPetroleum/Models/Entities/Position.cs
@@ -11,9 +11,7 @@ namespace RedPetroleum.Models.Entities
         public Guid PositionId { get; set; }
 
         [Display(Name = "Наименование должности")]
-        [RegularExpression(@"^[a-zA-ZЁёӨөҮүҢңА-Яа-я -]+$", ErrorMessage = "Ввод цифр запрещен")]
         [StringLength(100, ErrorMessage = "Длина строки не должна превышать 100 символов")]
-        [Required(ErrorMessage = "Заполните поле!")]
         public string Name { get; set; }
         public ICollection<Employee> Employees { get; set; }        
     }
